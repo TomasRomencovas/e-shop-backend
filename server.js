3b6d1c12e13da9c3b6066e34bec38f677da08321
Tomas Romencovas <tomas.romencovas@gmail.com>
2025-04-24 18:15:59 +0300

    inisial

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello world" });
+});
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
